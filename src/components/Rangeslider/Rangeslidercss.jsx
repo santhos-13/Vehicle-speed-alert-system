887700bf86ad146685de5dc6e4dbedda09ed0fad
Santhosh <107121090@nitt.edu>
2023-01-05 13:12:02 +0530

    added range slider and some styling to web app

---
+++ src/components/Rangeslider/Rangeslidercss.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Rangeslider.css';
+
+
+
+function Rangeslider(){
+    const [value, setValue] = useState(50);
+    
+      const handleChange = (e) => {
+        setValue(e.target.value);
+        console.log(e.target.value);
+        };
+    return(
+
+
+        
+        
+        <div className="range-slider">
+            
+            <input type="range" min="0" max="200" value={value } className="range" id="myRange"  onChange={handleChange} onMouseMove={handleChange} />
+            <p> <span id="rangeValue">{value} </span><span id='unit'> km/hr</span></p>
+        </div>
+       
+       
+    
+
+    )
+}
+
+export default Rangeslider
