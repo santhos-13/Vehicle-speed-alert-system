c495926ade512e5ca2af04e28bb479fd2766bd47
Santhosh <107121090@nitt.edu>
2023-01-09 20:02:55 +0530

    added docker

---
+++ src/components/Icon.js
@@ -0,0 +1,12 @@
+
+
+import L from 'leaflet';
+// import marker from '../assets/placer.svg';
+const myIcon = new L.Icon({
+    iconUrl: './image/car-solid.svg',
+    iconRetinaUrl: marker,
+    popupAnchor:  [-0, -0],
+    iconSize: [32,45],     
+});
+
+export { iconPerson };
